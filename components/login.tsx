1cbcf2e109acf87590c36acfa1ecd77665070261
Wasin Watthanasrisong <wasinwatt@gmail.com>
2023-06-07 21:16:04 +0700

    add register note

--- components/login.tsx
+++ components/login.tsx
@@ -9,6 +9,10 @@ export default function PassageLogin() {
 
   return (
     <div className='border border-black rounded-lg w-[360px] mx-auto'>
+      <p className='text-sm mt-2 underline'>หากยังไม่เคย register กับระบบ</p>
+      <p className='text-sm underline'>
+        กดปุ่ม Register here ด้านล่างกล่องก่อนนะคะ
+      </p>
       <passage-auth
         app-id={process.env.NEXT_PUBLIC_PASSAGE_APP_ID}
       ></passage-auth>
